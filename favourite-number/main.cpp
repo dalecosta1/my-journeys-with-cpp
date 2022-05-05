4aeccd1cfa4ed359b72e9ec56b64601fe3142f1b
costa <costa@MBP-di-costa.lan>
2022-05-05 19:33:16 +0200

    first two very simple exercise

---
+++ favourite-number/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    int favNumber;
+    std::cout << "Enter your favourite number: " << std::endl;
+    std::cin >> favNumber;
+    std::cout << "Your favorite number is " << favNumber << std::endl;
+    return 0;
+}
